00a619e6737c429520180c72f7d9241890c055fa
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2021-09-26 10:23:11 +0600

    jspractice

---
+++ pracjs.js
@@ -0,0 +1,16 @@
+var firstName = "Shuvo";
+age= 25;
+var isMale = true;
+var isFemale = false;
+var surname= null;
+
+var person = {
+    name: firstName,
+    age: age,
+    dob: '01/01/4440'
+};
+
+person.age;
+person.name;
+person.dob;
+console.log(person);
