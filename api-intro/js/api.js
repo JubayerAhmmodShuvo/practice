4228a2835f876df74f175417905afaec638ec6ea
JubayerAhmmodShuvo <jubayer15-10807@diu.edu.bd>
2022-02-21 23:05:28 +0600

    jsom

--- api-intro/js/api.js
+++ api-intro/js/api.js
@@ -4,3 +4,21 @@ function loadData() {
         .then(response => response.json())
         .then(data => console.log(data))
 }
+
+function loadUsers() {
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => displayUser(data))
+}
+
+function loadPosts() {
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
+
+function displayUser(data) {
+    console.log(data)
+}
