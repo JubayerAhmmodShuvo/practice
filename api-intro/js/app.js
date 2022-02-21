96464289075079d98d4c69f8ba251bcc8f27904c
JubayerAhmmodShuvo <jubayer15-10807@diu.edu.bd>
2022-02-21 22:35:22 +0600

    j

--- api-intro/js/app.js
+++ api-intro/js/app.js
@@ -1,1 +1,24 @@
-console.log('jello  ');
+//Javascript Object Notation
+//JSON
+const user = { id: 11, name: 'gorib amir', job: 'actor' };
+
+//console.log(user);
+const stringified = JSON.stringify(user);
+//console.log(stringified);
+
+const shop = {
+    name: 'alia store',
+    address: 'ranbir road',
+    product: ['laptop', 'mobile', 'pepsi'],
+    owner: {
+        name: 'alia bhatt',
+        address: 'inida'
+    },
+    isExpensive: false
+};
+const shopStringified = JSON.stringify(shop);
+//console.log(shop);
+console.log(shopStringified);
+
+const converted = JSON.parse(shopStringified);
+console.log(converted);
