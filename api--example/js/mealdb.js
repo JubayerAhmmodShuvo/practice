097ba6c6af2955ef81fcf8363bd8221717c8f8c6
JubayerAhmmodShuvo <jubayer15-10807@diu.edu.bd>
2022-02-23 01:09:59 +0600

    api practice

--- api--example/js/mealdb.js
+++ api--example/js/mealdb.js
@@ -8,5 +8,25 @@ const searchFood = () => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     fetch(url)
         .then(response => response.json())
-        .then(data => console.log(data.meals))
+        .then(data => displaySearchResult(data.meals))
+}
+
+const displaySearchResult = meals => {
+    const searchResult = document.getElementById('search-result');
+    meals.forEach(meal => {
+        //console.log(meal);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">${meal.strInstructions.slice(0,200)}</p>
+                </div>
+            </div>
+`;
+        searchResult.appendChild(div);
+
+    })
 }
