5d90e30f7b3a4637dbffedaf3317e4c7d13ba800
JubayerAhmmodShuvo <jubayer15-10807@diu.edu.bd>
2022-02-22 23:37:23 +0600

    api practice

--- api--example/js/country.js
+++ api--example/js/country.js
@@ -1,5 +1,5 @@
 const loadCountries = () => {
-    fetch('https://restcountries.com/v2/all')
+    fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => displayCountries(data));
 }
@@ -12,13 +12,33 @@ const displayCountries = countries => {
 
         const div = document.createElement('div');
         div.classList.add('country');
-        const h3 = document.createElement('h3');
+        div.innerHTML = `
+        <h3>${country.name.common}</h3>
+        <p>${country.capital}</p>
+        <button onclick="loadCountryByName('${country.name.common}')">show details</button>
+        `
+            // const h3 = document.createElement('h3');
 
-        h3.innerText = country.name;
-        div.appendChild(h3);
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        div.appendChild(p);
+        // h3.innerText = country.name;
+        // div.appendChild(h3);
+        // const p = document.createElement('p');
+        // p.innerText = country.capital;
+        // div.appendChild(p);
         contriesDiv.appendChild(div);
     });
+}
+const loadCountryByName = name => {
+    const url = `https://restcountries.com/v3.1/name/${name}`;
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetail(data[0]));
+}
+const displayCountryDetail = country => {
+
+    const countryDiv = document.getElementById('country-detail');
+    countryDiv.innerHTML = `
+  <h5>${country.name.common}</h5>
+  <p>${country.population}</p><img src="${country.flags.png}" alt="">
+  `;
 }
