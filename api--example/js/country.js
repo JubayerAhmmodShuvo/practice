1b75ad1dfd4fca83bf7e30404615b530216ced0a
JubayerAhmmodShuvo <jubayer15-10807@diu.edu.bd>
2022-02-22 22:35:32 +0600

    api practice

---
+++ api--example/js/country.js
@@ -0,0 +1,24 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v2/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data));
+}
+loadCountries();
+const displayCountries = countries => {
+    const contriesDiv = document.getElementById('countries');
+
+
+    countries.forEach(country => {
+
+        const div = document.createElement('div');
+        div.classList.add('country');
+        const h3 = document.createElement('h3');
+
+        h3.innerText = country.name;
+        div.appendChild(h3);
+        const p = document.createElement('p');
+        p.innerText = country.capital;
+        div.appendChild(p);
+        contriesDiv.appendChild(div);
+    });
+}
