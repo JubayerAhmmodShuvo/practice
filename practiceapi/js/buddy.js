05d4f6e5517144a4f827af1fb2bacd3658157842
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-02-24 13:13:03 +0600

    practice

---
+++ practiceapi/js/buddy.js
@@ -0,0 +1,18 @@
+const loadBuddies = () => {
+    fetch('https://randomuser.me/api/?results=5000')
+        .then(response => response.json())
+        .then(data => displayBuddies(data));
+}
+loadBuddies();
+const displayBuddies = data => {
+    const buddies = data.results;
+    const buddiesDiv = document.getElementById('buddies');
+    for (const buddy of buddies) {
+
+
+        //console.log(buddy.email);
+        const p = document.createElement('p');
+        p.innerText = `Name:${buddy.name.title} ${buddy.name.first} ${buddy.name.last} email: ${buddy.email}`;
+        buddiesDiv.appendChild(p);
+    }
+}
