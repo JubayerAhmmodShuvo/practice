54ef27dc8cab5e4264064e2225e55cff4af0e7ad
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-02-25 15:35:58 +0600

    practicing

--- dd.js
+++ dd.js
@@ -1,6 +1,32 @@
-const add1= (num1, num2) => num1+num2 ;
-const sum= add1(11,22);
-console.log(sum);
+// const add1= (num1, num2) => num1+num2 ;
+// const sum= add1(11,22);
+// console.log(sum);
 
-const sum2 =add1(44,55);
-console.log(sum2);
+// const sum2 =add1(44,55);
+// console.log(sum2);
+// let p = 'Javascript';
+// let q = p;
+// p = 'React';
+// console.log(q);
+// const isTrue = 'false';
+// if (!isTrue) {
+//     console.log('hello');
+// } else {
+//     console.log('world');
+// }
+
+// function sum(p, q) {
+//     p + q;
+// }
+// const result = sum(2, 3);
+// console.log(result);
+// if ("2" === 2) {
+//     console.log("Inside if");
+// } else {
+//     console.log("Inside else");
+// }
+
+function work(x, y = 4) {
+    return x + y;
+}
+console.log(work(32));
