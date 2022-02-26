9a087706478a043027a1bbf8f3e9836413d62dd5
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-02-27 01:21:00 +0600

    doing

--- practiceapi/js/country.js
+++ practiceapi/js/country.js
@@ -1,7 +1,7 @@
 const loadCountries = () => {
     fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
-        .then(data => displayCountries(data));
+        .then(data => console.log(data));
 }
 loadCountries();
 const displayCountries = countries => {
--- practiceapi/js/countryapi.js
+++ practiceapi/js/countryapi.js
@@ -59,6 +59,9 @@ const loadCountryByName = () => {
     const searchResult = searchInput.value;
     searchInput.value = '';
 
+    const countriesDiv = document.getElementById('countries');
+    countriesDiv.innerHTML = '';
+
 
 
     const url = `https://restcountries.com/v3.1/name/${searchResult}`;
@@ -66,16 +69,30 @@ const loadCountryByName = () => {
     fetch(url)
         .then(res => res.json())
         .then(data => displayCountryDetail(data[0]));
-}
+};
 const displayCountryDetail = country => {
     const countryDiv = document.getElementById('country');
     countryDiv.innerHTML = `
-    <div class="card h-100">
-                    <img src="${country.flags.png}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Country Name: ${country.name.common}</h5>
-                        <p>Population: ${country.population}</p>
-                    </div>
-                </div>
+    <div class="card mb-3 mx-auto mt-5 h-100 w-50" >
+  <div class="row">
+    <div class="col-md-4">
+      <img src="${country.flags.png}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Country Name: ${country.name.common}</h5>
+        <p class="card-text">Population: ${country.population}</p>
+        <p class="card-text">Po: ${country.region}</p>
+        <p class="card-text">Status: ${country.status}</p>
+        <p class="card-text">Capital: ${country.capital}</p>
+        <p class="card-text">Capital: ${country.currencies[Object.keys(country.currencies)[0]].name}</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+ 
   `;
-}
+};
