d8d622889a24871292af072166e2420e31b425d9
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-02-28 16:00:23 +0600

    done

--- practiceConcept/app.js
+++ practiceConcept/app.js
@@ -1,18 +1,29 @@
 const allPlayers = () => {
-    const searchValue = document.getElementById('search-box').value;
-    const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`;
+    document.getElementById('player-container').innerHTML = '';
+    document.getElementById('spinner').style.display = 'block';
+    const searchValue = document.getElementById('search-box');
+    const searchText = searchValue.value;
+    searchValue.value = ``;
+    const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchText}`;
     fetch(url)
         .then(response => response.json())
         .then(data => showPlayersDetails(data.player));
 
+
+
 };
 const showPlayersDetails = (players) => {
+    if (players) {
+        document.getElementById('spinner').style.display = 'none';
+    } else {
+        document.getElementById('spinner').style.display = 'block';
+    }
     for (const player of players) {
         const parent = document.getElementById('player-container');
         const div = document.createElement('div');
         div.innerHTML = `<div class="card border p-5">
                         <div class="pro-pic">
-                            <img class="w-25" src="${player.strThumb}" alt="">
+                            <img class="w-50" src="${player.strThumb}" alt="">
                         </div>
                         <h2>Name:${player.strPlayer}</h2>
                         <h5>Country:${player.strNationality}</h5>
@@ -38,5 +49,10 @@ const details = (id) => {
 };
 
 const setDetails = (info) => {
-    console.log(info);
-}
+    document.getElementById('details-container').innerHTML = `
+  <div>
+   <img src="" alt="">
+   <h1> Name:${info.strPlayer}</h1>
+  </div>  
+  `;
+};
