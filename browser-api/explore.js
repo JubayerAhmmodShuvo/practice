778c658a42d5f4804da2bd28b8098fdbff690fa1
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-03 22:41:35 +0600

    tada

---
+++ browser-api/explore.js
@@ -0,0 +1,10 @@
+const good = () => {
+    alert(`Good`);
+};
+const hood = () => {
+    const response = confirm('Are you sure you want to go to the ? ? ');
+    console.log(response);
+    if (response === true) {
+        alert(`You are in the hood`);
+    } else { 'G DN ' };
+};
