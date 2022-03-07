b409f182d28320bee46e0e58c09b194e345dbd64
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-08 01:10:31 +0600

    practicing

---
+++ arraymethods.js
@@ -0,0 +1,26 @@
+const product = [{
+        name: 'laptop',
+        price: 3200,
+        brand: 'lenovo',
+        color: 'black',
+    },
+    {
+        name: 'phone',
+        price: 3200,
+        brand: 'lenovo',
+        color: 'cyan',
+    },
+    {
+        name: 'watch',
+        price: 3200,
+        brand: 'lenovo',
+        color: 'yellow',
+    },
+    {
+        name: 'sunglass',
+        price: 3200,
+        brand: 'lenovo',
+        color: 'gray',
+    }
+];
+const brands = product.map(product => product.brand);
