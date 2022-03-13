fb44cec23990443d89cb9b04e1abff790dcd6b0b
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-13 15:14:23 +0600

    enter search added

--- practiceapi/js/meals.js
+++ practiceapi/js/meals.js
@@ -1,3 +1,13 @@
+const searchBtn = document.getElementById("button-search");
+const searchInput= document.getElementById("search-field"); 
+searchInput.addEventListener("keypress", function (event) {
+   // event.preventDefault();
+    if (event.keyCode == 13) {
+        searchBtn.click();
+    }
+
+});
+
 const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
