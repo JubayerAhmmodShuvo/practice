ffc163086aa9315a1a063a20e2090e64be65625b
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-15 00:02:19 +0600

    recap part

--- rest-countries-recap/js/countries.js
+++ rest-countries-recap/js/countries.js
@@ -5,6 +5,20 @@ const loadCountries = () => {
 }
 
 const displayCountries = countries => {
-  console.log(countries);
+  
+  const allCountriesHTML = countries.map(country => getCountryHTML(country));
+ // console.log(allCountriesHTML);
+  const container = document.getElementById('countries');
+  container.innerHTML = allCountriesHTML.join(' ');
 }
+const getCountryHTML = country => {
+  return `
+  <div>
+  <h2>${country.name.common}</h2>
+  <img src="${country.flags.png}" alt="${country.name.common} flag">
+  </div>
+  `
+}
+
+
 loadCountries();
