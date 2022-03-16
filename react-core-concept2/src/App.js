f0435204b4e80cfdaeb43cc69445901e1f8c114c
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-16 14:41:23 +0600

    practicing

--- react-core-concept2/src/App.js
+++ react-core-concept2/src/App.js
@@ -1,15 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
  <div className="App">
-    {/* <Counter></Counter> */}
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
+function ExternalUsers() {
+  const [ users , setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+  }, []);
+  return (
+    <div>
+      <h1>External Users</h1>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name= {user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
 
+function User(props) {
+  return (
+    <div style={{border: '4px solid red',margin:'20px',borderRadius:'25px'}}>
+      <h3>Name: {props.name}</h3>
+      <p>Email:{props.email}</p>
+    </div>
+  )
+}
 
 
 
