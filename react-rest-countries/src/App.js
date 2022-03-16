a4e4ac9dd875e3ed322368a09c23e052e6762448
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-16 22:08:47 +0600

    practicing

---
+++ react-rest-countries/src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+   <LoadCountries />
+    </div>
+  );
+}
+
+function LoadCountries() {
+  const [countries, setCountries] = useState([]);
+  
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(response => response.json())
+      .then(data => setCountries(data));
+  },[])
+  return (
+    <div>
+      <h1>Visiting every Country of the world</h1>
+      <h3>Available Counties:{countries.length}</h3>
+      {
+        countries.map(country => <Country name= {country.name.common} population= {country.population}></Country>)
+      }
+
+    </div>
+  )
+}
+
+function Country(props) {
+  return (
+    <div>
+      <h2>Name:{props.name}</h2>
+      <h4>Population:{ props.population}</h4>
+    </div>
+  )
+}
+
+export default App;
