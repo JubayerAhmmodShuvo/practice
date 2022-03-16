403d3c3b3134cb91d88bfb90fb5e70bbaa00d322
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-16 14:19:49 +0600

    practicing

--- react-core-concept2/src/App.js
+++ react-core-concept2/src/App.js
@@ -1,21 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-     <Product name="laptop" ></Product>
+ <div className="App">
+    {/* <Counter></Counter> */}
     </div>
   );
 }
 
-function Product(props) {
+
+
+
+function Counter() {
+  const [count, setCount] = useState(55);
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+  
+
   return (
-    <div className="product" >
-      <h3>Name:{props.name} </h3>
-      <p>Price:</p>
+    <div>
+      <h1>Count:{count} </h1>
+      <button onClick={increaseCount}>Incrase</button> <br />
+      <button onClick={decreaseCount}>DEcrase</button>
     </div>
   )
 }
 
+// function Product(props) {
+//   return (
+//     <div className="product" >
+//       <h3>Name:{props.name} </h3>
+//       <p>Price:</p>
+//     </div>
+//   )
+// }
+
+ /*  <div className="App">
+     <Product name="laptop" ></Product>
+    </div> */
 export default App;
