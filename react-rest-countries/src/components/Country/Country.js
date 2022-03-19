fc353a07b5667dbfce44faa7279e9b0ebcdc9fe0
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-19 17:59:21 +0600

    practicing

--- react-rest-countries/src/components/Country/Country.js
+++ react-rest-countries/src/components/Country/Country.js
@@ -3,12 +3,13 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
+  const { area } = props.country;
   return (
     <div className='country'>
       <img src={ props.country.flags.png} alt="" />
       <h2>Name :{props.name} </h2>
       <h4>Population : {props.population}</h4>
-      <h5> <small>Area : {props.country.area}</small> 
+      <h5> <small>Area : {area}</small> 
       </h5>
       <h4> <small>Region : {props.country.region}</small></h4>
 
