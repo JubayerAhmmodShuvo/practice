35c7b6b6b827eba92fe60f2edae156827971cecc
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-21 00:03:59 +0600

    working with localstorage

--- react-storage-and-handler/src/Utilities/fakedb.js
+++ react-storage-and-handler/src/Utilities/fakedb.js
@@ -1,14 +1,30 @@
 //use localStorage to manage cart data and
 
 const addToDb = id => {
-  const quantity = localStorage.getItem(id);
-  if (quantity) {
-    localStorage.setItem(id, parseInt(quantity) + 1);
-  } else {
-    localStorage.setItem(id, 1);
-  } 
+  let shoppingCart = {};
+ //get the shoppingCart
+  const storedCart = localStorage.getItem('shoppingCart');
+  if (storedCart) { 
+    shoppingCart = JSON.parse(storedCart);
+  }
+  else {
+    shoppingCart = {};
+  }
 
-  localStorage.setItem(id)
+  const quantity = shoppingCart[id];
+  if (quantity) {
+    const newQuantity = quantity + 1;
+    shoppingCart[id] = newQuantity;
+
+    //localStorage.setItem(id, parseInt(quantity) + 1);
+  //  localStorage.setItem(id, parseInt(quantity) + 1)
+  } else {
+    shoppingCart[id] = 1;
+   // localStorage.setItem(id, 1);
+  } 
+   localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+
+  
 }
 
 export { addToDb };
