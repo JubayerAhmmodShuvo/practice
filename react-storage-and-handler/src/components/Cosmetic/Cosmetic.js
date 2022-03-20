0db7cd241028a7e543379c199a9a5f290aef4916
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-20 22:17:28 +0600

    own json creatd

--- react-storage-and-handler/src/components/Cosmetic/Cosmetic.js
+++ react-storage-and-handler/src/components/Cosmetic/Cosmetic.js
@@ -1,14 +1,21 @@
 import React from 'react';
-
+import './Cosmetic.css'
 const Cosmetic = (props) => {
-  const { name,price} = props.cosmetic;
+  const { name, price,id } = props.cosmetic;
+  const addToCart = (id) => {
+    console.log('item added',id)
+  }
+  const addToCartWithParam=()=>addToCart(id);
   return (
-    <div>
+    <div className='product' >
       {/* <h2>Buy This :{ props.cosmetic.name}</h2>
       <p>Only FOr:{ props.cosmetic.price} </p> */}
       <h2>Buy This :{ name}</h2>
-      <p>Only For:{ price} </p>
+      <p>Only For:{price} </p>
+      <button onClick={addToCartWithParam} >Add TO cart</button>
+      <button onClick={()=>addToCart(id)} >Add TO cart:Shortcut</button>
     </div>
+    
   );
 };
 
