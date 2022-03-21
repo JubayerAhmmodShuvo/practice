40fd098d0dae2e24360854f335076e593dda9634
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-22 01:00:56 +0600

    cart done

--- ema-john-simple/src/components/Shop/Shop.js
+++ ema-john-simple/src/components/Shop/Shop.js
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
-  useEffect(() => { 
+  useEffect(() => {
     fetch('products.json')
       .then(res => res.json())
       .then(data => setProducts(data))
-  },[])
+  }, []);
   return (
     <div className="shop-container">
       <div className="products-container">
-        <h3>This is for products: {products.length}</h3>
+        {
+          products.map(product => <Product
+            key={product.id}
+            product={product} />)
+        }
       </div>
       <div className="cart-container">
       <h4>Order Summary</h4>
