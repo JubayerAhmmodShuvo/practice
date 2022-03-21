bfeac4ad4d52436d51e8d6a306b5ad930b9989f2
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-21 23:36:36 +0600

    api fetched

--- ema-john-simple/src/components/Header/Header.js
+++ ema-john-simple/src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
       <nav className="header">
         <img src={logo} alt="" />
         <div>
-          <a href="/home">Home</a>
+          <a href="/shop">Shop</a>
           <a href="/orders">Orders</a>
           <a href="/inventory">Inventory</a>
           <a href="/about">About</a>
