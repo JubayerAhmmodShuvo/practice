ee180aa2375936f4c2a47c22603b09debfa6e2a0
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-29 00:26:45 +0600

    routing practice

--- react-router/src/components/Friends/Friends.js
+++ react-router/src/components/Friends/Friends.js
@@ -1,13 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
   const [friends, setFriends] = useState([]);
   useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setFriends(data));
     
   },[])
   return (
     <div>
       <h3>Hello Friends How are Your Mashoka</h3>
+      <p>Parle kisu taka dhar dis...{friends.length}</p>
+      {
+        friends.map(friend => <Friend 
+          friend={friend}
+          key={friend.id}
+        />)
+      }
     </div>
   );
 };
