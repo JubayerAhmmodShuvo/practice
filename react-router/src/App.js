ee165c38c4c39c394de003111352e38e9156e041
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-28 22:50:09 +0600

    simple routing are going

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,11 +1,24 @@
 
+import { Route,Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Friends from './components/Friends/Friends';
 import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
+
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <h1>Welcome to my fancy routing website</h1>
+      <Routes>
+      
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/friends" element={<Friends />}></Route>
+        <Route path="*" element={<NotFound />} ></Route>
+      </Routes>
+
     </div>
   );
 }
---
+++ react-router/src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h2>The page you are looking for is not found</h2>
+    </div>
+  );
+};
+
+export default NotFound;
