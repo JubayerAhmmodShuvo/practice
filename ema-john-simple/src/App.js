c3b56cb9ddeb3b0d1323d842fdcc1509c0ddfacd
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-30 21:28:15 +0600

    routing practicing

--- ema-john-simple/src/App.js
+++ ema-john-simple/src/App.js
@@ -1,6 +1,7 @@
 
 import { Route, Routes } from 'react-router';
 import './App.css';
+import About from './components/About/About';
 import Header from './components/Header/Header';
 import Inventory from './components/Inventory/Inventory';
 import Orders from './components/Orders/Orders';
@@ -17,6 +18,7 @@ function App() {
         <Route path='/shop' element={<Shop></Shop>} ></Route>
         <Route path='/orders' element={<Orders />}></Route>
         <Route path='/inventory' element={<Inventory />}></Route>
+        <Route path='/about' element={<About />}></Route>
       </Routes>
      
     </div>
