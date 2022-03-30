b2ea8a91679d44d2f0ab8d2829de302f1108d74d
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-03-30 22:25:59 +0600

    routing practicing

--- ema-john-simple/src/components/Orders/Orders.js
+++ ema-john-simple/src/components/Orders/Orders.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 
 const Orders = () => {
-  const [products,setProducts]=useProducts();
+  const [products, setProducts] = useProducts();
+  const [cart, setCart] = useCart(products);
   return (
     <div>
       <h3>This is Orders:{products.length}</h3>
+      <p>cart has:{ cart.length}</p>
     </div>
   );
 };
