4aa873e0c0bfab5eb1d56ca0dcecd57d17e64c4f
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-04-06 11:18:30 +0600

    signin and signout button used with ternary operator

--- simple-firebase-authentication/src/App.js
+++ simple-firebase-authentication/src/App.js
@@ -2,15 +2,19 @@
 import './App.css';
 import app from './firebase.init';
 import { getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import { useState } from 'react';
 
 const auth  =getAuth(app);
 function App() {
+  const [user, setUser] = useState({});
+
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () => { 
     signInWithPopup(auth, provider)
       .then(result => {
         const user = result.user;
+        setUser(user);
         console.log(user);
       
       })
@@ -20,9 +24,26 @@ function App() {
 
   }
 
+  const handleGoogleSignOut = () => { 
+    auth.signOut()
+      .then(() => {
+        setUser({});
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn} >Google sign In</button>
+      {
+      user.email ?  <button onClick={ handleGoogleSignOut} >SignOUt</button>  : <button onClick={handleGoogleSignIn} >Google sign In</button>
+      }
+      
+     
+      <h2>Name: {user.displayName}</h2>
+      <p>I know your email: {user.email}</p>
+      <img src={ user.photoURL} alt="" />
     </div>
   );
 }
