bb98e72002310285e638bcbcf997537ffc269196
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-04-11 00:46:56 +0600

    making responsive

--- genuis-car-app/src/Pages/Home/Service/Service.js
+++ genuis-car-app/src/Pages/Home/Service/Service.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './Service.css'
 
 const Service = ({ service }) => {
   const { name,img, description, price } = service;
   return (
-    <div>
+    <div className="service" >
       <img src={ img} alt="" />
-      <h2>{ name}</h2>
+      <h2>{name}</h2>
+      <h6>{price}</h6>
+      <p>{description}</p>
+      <button>Book: { name}</button>
     </div>
   );
 };
--- genuis-car-app/src/Pages/Home/Services/Services.js
+++ genuis-car-app/src/Pages/Home/Services/Services.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import Service from '../Service/Service';
+import './Services.css';
 
 
 const Services = () => {
@@ -15,13 +16,12 @@ const Services = () => {
 
   return (
     <div>
-      <h2>Servie: {services.length}</h2>
-      {
-        services.map(service => <Service
-          key={service.id}
-        service={service}
-        ></Service>)
-      }
+      <h2 className="service-title" >Servie: {services.length}</h2>
+      <div className="services-container">
+        {services.map((service) => (
+          <Service key={service.id} service={service}></Service>
+        ))}
+      </div>
     </div>
   );
 };
