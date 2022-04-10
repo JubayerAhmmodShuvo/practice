34e178c261bf30df0ac740ef9c29ac4f07ac2270
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-04-10 11:58:33 +0600

    form is still in under process :)

--- ema-john-simple/src/components/Login/Login.js
+++ ema-john-simple/src/components/Login/Login.js
@@ -10,15 +10,20 @@ const Login = () => {
         <form action="">
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="" />
+            <input type="email" name="email" id="" required />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="" />
+            <input type="password" name="password" id="" required />
           </div>
           <input className="form-submit" type="submit" value="Login" />
         </form>
-        <p>New To Ema-John? <Link className="form-link" to="signup">Create One</Link> </p>
+        <p>
+          New To Ema-John?{" "}
+          <Link className="form-link" to="/signup">
+            Create One
+          </Link>{" "}
+        </p>
       </div>
     </div>
   );
--- ema-john-simple/src/components/SignUp/SignUp.js
+++ ema-john-simple/src/components/SignUp/SignUp.js
@@ -1,10 +1,64 @@
-import React from 'react';
-import './SignUp.css';
+import React, { useState } from "react";
+import "./SignUp.css";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleEmailBlur = event => { 
+    setEmail(event.target.value);
+  }
+  const handlePasswordBlur = event => {
+    setPassword(event.target.value);
+  }
+  const handleConfirmPasswordBlur = event => {
+    setConfirmPassword(event.target.value);
+  }
+  const handeCreateUser = event => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    } else { 
+
+    }
+  }
   return (
-    <div>
-      <h2>SignUp</h2>
+    <div className="form-container">
+      <div className="">
+        <h3 className="form-title">SignUp</h3>
+        <form onSubmit={handleCreateUder} >
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              onBlur={handleEmailBlur}
+              type="email"
+              name="email"
+              id=""
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Confirm Password</label>
+            <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm password" id="" required />
+          </div>
+          <p style={{color:"red"}} >{ error}</p>
+          <input className="form-submit" type="submit" value="Login" />
+        </form>
+        <p>
+          Already Have an Account?{" "}
+          <Link className="form-link" to="/login">
+            Create One
+          </Link>{" "}
+        </p>
+      </div>
     </div>
   );
 };
