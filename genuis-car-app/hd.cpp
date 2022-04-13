3ecf77f53b193848f28ab75273bf3aa1b0c50a9a
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-04-13 11:54:47 +0600

    practicing

--- genuis-car-app/hd.cpp
+++ genuis-car-app/hd.cpp
@@ -1,20 +1,5 @@
-
-/* 
-You will get an array of n numbers. Find out the k-th maximum number of the array.
-
-Suppose you have 9 2 5 1 10. Here 1st maximum number is 10, second is 9, third is 5 and so on.
-
-Input
-First line contain n and k (1 ≤ n ≤ 1000, 1 ≤ k ≤ n) The second line contains n space-separated integers ai (0 ≤ ai ≤ 100), ai is the i-th number of the array.
-
-Output
-Print the k-th maximum number of the array.
-
- */
+ //Print “Yes” if the Friend Array is sorted in descending order otherwise “No”.
 
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
 
 
--- genuis-car-app/src/Pages/Shared/Header/Header.js
+++ genuis-car-app/src/Pages/Shared/Header/Header.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header>
-      <h2>This is header</h2>
-    </header>
+    <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark" sticky='top' className="mb-5">
+      <Container>
+        <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Login
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
