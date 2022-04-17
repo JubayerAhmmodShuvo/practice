6bc26d78012761e40aa716dfda5ffa3e412b994e
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-04-17 19:44:47 +0600

    private auth completed

--- genuis-car-app/src/Pages/RequireAuth/RequireAuth.js
+++ genuis-car-app/src/Pages/RequireAuth/RequireAuth.js
@@ -5,7 +5,7 @@ import auth from '../../firebase.init';
 
 const RequireAuth = ({ children }) => {
   const [user] = useAuthState(auth);
-  const location=useLocation();
+  const location=useLocation(); 
   if (!user) {
     return <Navigate to="/login" stare={{from: location}} replace />;
   }
