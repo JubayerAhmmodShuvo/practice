f9c54bfc9e46c36ce7d061255ec44ea00ff20aae
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-04-21 18:05:57 +0600

    practicing mongo

--- node-mongo-crud/index.js
+++ node-mongo-crud/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+const ObjectId= require("mongodb").ObjectId;
 
 app.use(cors());
 app.use(express.json());
@@ -37,6 +38,29 @@ async function run() {
       const result = await collection.insertOne(newUser);
       res.send(result);
     });
+    app.get('/user/:id', async (req, res) => { 
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const user = await collection.findOne(query);
+      res.send(user);
+    })
+
+    app.put('/user/:id', async (req, res) => { 
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const newUser = req.body;
+      const result = await collection.updateOne(query, {$set: newUser});
+      res.send(result);
+    })
+
+    app.delete("/user/:id", async (req, res) => { 
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await collection.deleteOne(query);
+
+        res.send(result);
+    });
+
   } finally {
     //await client.close();
   }
