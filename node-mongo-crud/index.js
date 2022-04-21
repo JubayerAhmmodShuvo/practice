6a66751145d730c32e057e48c577200fe3a3bb3b
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-04-21 17:13:18 +0600

    problem in mongo solved

--- node-mongo-crud/index.js
+++ node-mongo-crud/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,8 +9,6 @@ app.use(express.json());
 
 //DpDtGJuYbaVAJN8f
 //dbuser1
-
-
 
 const uri =
   "mongodb+srv://dbuser1:DpDtGJuYbaVAJN8f@cluster0.aimhj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -22,24 +20,31 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const collection=client.db("myFirstDatabase").collection("myFirstCollection");
-    const user = { name: 'mahini ni', email: "mahi@gmail.com" }
-    const result = await collection.insertOne(user);
-    console.log(result.insertedId);
-    
-  }
-  finally {
-    await client.close();
-  }
+    const collection = client
+      .db("myFirstDatabase")
+      .collection("myFirstCollection");
+     app.get('/user',async (req, res) =>
+    {
+      const query = {};
+      const cursor = collection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+   }) 
 
+    app.post("/user", async (req, res) => {
+      const newUser = req.body;
+      console.log("added new user", newUser);
+      const result = await collection.insertOne(newUser);
+      res.send(result);
+    });
+  } finally {
+    //await client.close();
+  }
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`)
-);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
