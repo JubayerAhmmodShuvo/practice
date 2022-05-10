99c98587153b81ed9178616cf8417246df122d02
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-05-10 12:23:56 +0600

    final practice m 1

---
+++ doctors-portal/src/Pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Banner from '../Home/Banner';
+import Info from './Info';
+
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <Info />
+    </>
+  );
+};
+
+export default Home;
