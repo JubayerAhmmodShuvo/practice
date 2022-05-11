82b95833778e34f9abecb8a3f57fd77f205f8a58
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-05-11 14:50:45 +0600

    final practice m 2

--- doctors-portal/src/Pages/Appointment/Service.js
+++ doctors-portal/src/Pages/Appointment/Service.js
@@ -1,21 +1,32 @@
 import React from 'react';
 
-const Service = ({ service }) => {
+const Service = ({ service, setTreatment }) => {
   const { name, slots } = service;
   return (
     <div>
       <div class="card lg:max-w-lg bg-base-100 shadow-xl">
         <div class="card-body">
           <h2 class="card-title text-secondary">{name}</h2>
-          <p>{ 
-            slots.length>0 ?
+          <p>
+            {slots.length > 0 ? (
               <span>{slots[0]}</span>
-              :
-              <span className='text-red-500' >No slot availabe</span>
-          }</p>
-          <p>P{ slots.length} {slots.length >1 ? "spaces" : "space"} available</p>
+            ) : (
+              <span className="text-red-500">No slot availabe</span>
+            )}
+          </p>
+          <p>
+            P{slots.length} {slots.length > 1 ? "spaces" : "space"} available
+          </p>
           <div class="card-actions justify-center">
-            <button disabled={slots.length===0} class="btn btn-secondary">Book Now</button>
+           
+            <label
+              disabled={slots.length === 0}
+              onClick={() => setTreatment(service)}
+              for="booking-modal"
+              class="btn btn-secondary text-white uppercase"
+            >
+              Book Apointment
+            </label>
           </div>
         </div>
       </div>
