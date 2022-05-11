b5248432797b662fd42a99ebc28f53702732e34c
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-05-11 11:26:02 +0600

    final practice m 1

---
+++ doctors-portal/src/Pages/Appointment/AppointmentBanner.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import chair from '../../assets/images/chair.png';
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import { format } from 'date-fns';
+
+const AppointmentBanner = () => {
+  const [date,setDate]=useState(new Date());
+  return (
+    <div>
+      <div class="hero min-h-screen  ">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt="" />
+          <div>
+            <DayPicker mode="single" selected={date} onSelect={setDate} />
+            <p>You Have selected: {format(date, "PP")}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AppointmentBanner;
