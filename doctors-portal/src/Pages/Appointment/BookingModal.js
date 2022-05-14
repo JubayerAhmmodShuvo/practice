dc8436babec90f016e926930e7326940f401c686
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-05-14 09:37:27 +0600

    from f-3

--- doctors-portal/src/Pages/Appointment/BookingModal.js
+++ doctors-portal/src/Pages/Appointment/BookingModal.js
@@ -1,8 +1,12 @@
 import { format } from 'date-fns';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const BookingModal = ({ date, treatment, setTreatment }) => {
   const { name, slots } = treatment;
+   const [user, loading, error] = useAuthState(auth);
+
   const handleBooking = (e) => {
     e.preventDefault();
     const slot = e.target.slot.value;
@@ -39,11 +43,14 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
             <input
               type="text"
               placeholder="Your Name"
+              disabled
+              value={user?.displayName || ""}
               name="name"
               class="input input-bordered w-full max-w-xs"
             />
             <input
               type="Email"
+              disabled value={user?.email || ''}
               name="email"
               placeholder="Email Address"
               class="input input-bordered w-full max-w-xs"
