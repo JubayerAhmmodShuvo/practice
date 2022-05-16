e2ac8d770ad133ae16535c9cffe237b13f51c3db
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-05-16 19:22:17 +0600

    before heroku

--- server-doctors-portal/index.js
+++ server-doctors-portal/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
@@ -9,12 +10,34 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hx876.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ error: "Unauthorized Access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ error: "Forbidden" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+
+
+  }
+
+
 async function run() {
   try {
     await client.connect();
@@ -25,6 +48,9 @@ async function run() {
     const bookingCollection = client
       .db("doctors_portal")
       .collection("bookings");
+    const usersCollection = client
+      .db("doctors_portal")
+      .collection("users");
 
     /**
      * API Naming Convention
@@ -36,11 +62,20 @@ async function run() {
     
      */
 
-    app.get('/booking', async (req, res) => { 
-      const patient=req.query.patient;
-      const query = { patient: patient };
-      const bookings = await bookingCollection.find(query).toArray();
-      res.send(bookings);
+    app.get('/booking',verifyJWT, async (req, res) => { 
+      const patient = req.query.patient;
+      const decodedEmail = req.decoded.email;
+      if (patient === decodedEmail) {
+        const query = { patient: patient };
+        const bookings = await bookingCollection.find(query).toArray();
+        res.send(bookings);
+      }
+      else { 
+       return res.status(403).send({ error: "Forbidden" });
+      }
+      
+
+   
 
     })
 
@@ -58,6 +93,51 @@ async function run() {
       const result = await bookingCollection.insertOne(booking);
       return res.send({success:true, result});
     });
+
+    app.get('/user', verifyJWT, async (req, res) => {
+      const users = await usersCollection.find().toArray();
+      res.send(users);
+    })
+     app.get("/admin/:email", async (req, res) => {
+       const email = req.params.email;
+       const user = await usersCollection.findOne({ email: email });
+       const isAdmin = user.role === "admin";
+       res.send({ admin: isAdmin });
+     });
+
+     app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+       const email = req.params.email;
+       const requester = req.decoded.email;
+       const requesterAccount = await usersCollection.findOne({
+         email: requester,
+       });
+       if (requesterAccount.role === "admin") {
+         const filter = { email: email };
+         const updateDoc = {
+           $set: { role: "admin" },
+         };
+         const result = await usersCollection.updateOne(filter, updateDoc);
+         res.send(result);
+       } else {
+         res.status(403).send({ message: "forbidden" });
+       }
+     });
+
+    app.put('/user/:email', async (req, res) => {
+      const emial = req.params.email;
+      const user = req.body;
+      const filter = { email: emial };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: emial }, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+       res.send({result,token:token});
+      
+    })
 
     app.get("/service", async (req, res) => {
       const query = {};
